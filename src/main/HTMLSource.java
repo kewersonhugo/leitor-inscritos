8227289ca44a878ca28230499d77abe6e84c6dd5
Handerson Frota <handersonbf@gmail.com>
2014-09-30 11:21:43 -0300

    gerando o html

---
+++ src/main/HTMLSource.java
@@ -0,0 +1,19 @@
+package main;
+
+public class HTMLSource {
+
+	private StringBuilder stringsHTML = new StringBuilder();
+	
+	public HTMLSource(){
+		stringsHTML.append("<table>").append("<tr><td>Nome</td><td>Blusa</td></tr>");
+	}
+	
+	public void setarValores(String nome, String tamanhoBlusa){
+		stringsHTML.append("<tr><td>").append(nome).append("</td>");
+		stringsHTML.append("<td>").append(tamanhoBlusa).append("</td>").append("</tr>");
+	}
+	public String geraHTML(){
+		stringsHTML.append("</table>");
+		return stringsHTML.toString();
+	}
+}
