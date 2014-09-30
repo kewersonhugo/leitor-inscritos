f555d5a31a2e3aad643248126ab76cdf9deb91b1
gabrielmapurunga <gabrielmapurunga@gmail.com>
2014-09-30 12:19:34 -0300

    Resolvido o problema de encode

---
+++ src/main/HTMLSource.java
@@ -0,0 +1,20 @@
+package main;
+
+public class HTMLSource {
+
+	private StringBuilder stringsHTML = new StringBuilder();
+	
+	public HTMLSource(){
+		stringsHTML.append("<table>").append("<tr><td>Número</td><td>Nome</td><td>Blusa</td></tr>");
+	}
+	
+	public void setarValores(Integer numero, String nome, String tamanhoBlusa){
+		stringsHTML.append("<tr><td>").append(numero).append("</td>");
+		stringsHTML.append("<tr><td>").append(nome).append("</td>");
+		stringsHTML.append("<td>").append(tamanhoBlusa).append("</td>").append("</tr>");
+	}
+	public String geraHTML(){
+		stringsHTML.append("</table>");
+		return stringsHTML.toString();
+	}
+}
--- src/main/Leitor.java
+++ src/main/Leitor.java
@@ -1,12 +1,17 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
 
 public class Leitor {
 
@@ -19,18 +24,25 @@ public class Leitor {
 			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
 			br = new BufferedReader(isr);
 			String textoDoArquivo = br.readLine();
-
+			int cont = 0;
+			HTMLSource html = new HTMLSource();
+			textoDoArquivo = br.readLine();
+			
 			while (textoDoArquivo != null) {
-				textoDoArquivo = br.readLine();
 				String[] textoDoArquivoSeparado = textoDoArquivo.split(",");
-				System.out.println(textoDoArquivoSeparado[24] + " "
-						+ textoDoArquivoSeparado[25] + " / "
-						+ textoDoArquivoSeparado[22] + ": "
-						+ textoDoArquivoSeparado[23]);
-
+				html.setarValores(new Integer(cont),
+						textoDoArquivoSeparado[28], textoDoArquivoSeparado[26]);
+				cont++;
 				textoDoArquivo = br.readLine();
 			}
 
+			OutputStream os = new FileOutputStream("inscritos.html", false);
+			OutputStreamWriter osw = new OutputStreamWriter(os,"ISO-8859-1");
+			BufferedWriter bw = new BufferedWriter(osw);
+			bw.newLine();
+			bw.write(html.geraHTML());
+			bw.write("Total: " + cont);
+			bw.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Não foi possível encontrar o arquivo.");
 			e.printStackTrace();
